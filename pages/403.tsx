dc241df5f478763124800affb2d3ff00a83311ba
SaktiDw <sakti596@gmail.com>
2022-12-14 19:19:15 +0800

    error page

--- pages/403.tsx
+++ pages/403.tsx
@@ -1,12 +1,15 @@
+import { ErrorLayout } from "@/components";
 import { useRouter } from "next/router";
 
 // pages/403.js
 export default function Custom403() {
   const router = useRouter();
   return (
-    <h1>
-      <span onClick={() => router.back()}>Click here to go back</span>403 -
-      Unauthorized
-    </h1>
+    <ErrorLayout>
+      <h1>
+        <span onClick={() => router.back()}>Click here to go back</span>403 -
+        Unauthorized
+      </h1>
+    </ErrorLayout>
   );
 }
